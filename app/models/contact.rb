bb1a98637c65f514689a2d03c314952d805344be
Jake Anderson <andesitedesign@gmail.com>
2017-03-22 11:31:52 -0500

    Add contact validations with tests

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,19 @@
 class Contact < ApplicationRecord
-  validates :first_name, presence: true
-  validates  :last_name, presence: true
+  VALID_NAME_REGEX = /[a-zA-Z]/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email.downcase! }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX },
+                         length: { minimum: 2 }
+  validates  :last_name, presence: true, format: { with: VALID_NAME_REGEX },
+                         length: { minimum: 2 }
+  validates      :email, presence: true, length: { maximum: 255 },
+                         format: { with: VALID_EMAIL_REGEX },
+                         uniqueness: { case_sensitive: false }
+  validates      :phone, length: { is: 10 }, numericality: { only_integer: true },
+                         allow_nil: true
+  validates  :phone_alt, length: { is: 10 }, numericality: { only_integer: true },
+                         allow_nil: true
+  validates      :state, length: { is: 2 }
+  validates        :zip, length: { is: 5 }, numericality: { only_integer: true },
+                         allow_nil: true
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,11 +4,18 @@ RSpec.describe Contact, type: :model do
   fixtures :contacts
 
   before do
-    @contact = contacts(:stark)
+    @contact  = contacts(:stark)
+    @contact2 = contacts(:parker)
   end
 
   it 'should be valid' do
     expect(@contact.valid?).to eq(true)
+    expect(@contact2.valid?).to eq(true)
+  end
+
+  it 'should prevent saving with no info' do
+    @contact = Contact.new
+    expect(@contact.valid?).to eq(false)
   end
 
   it 'should have first name' do
@@ -19,5 +26,71 @@ RSpec.describe Contact, type: :model do
   it 'should have last name' do
     @contact.last_name = "   "
     expect(@contact.valid?).to eq(false)
+  end
+
+  it 'should have at least 2 chars in names' do
+    @contact.first_name = "a"
+    expect(@contact.valid?).to eq(false)
+
+    @contact.first_name = "aa"
+    expect(@contact.valid?).to eq(true)
+
+    @contact.last_name = "b"
+    expect(@contact.valid?).to eq(false)
+
+    @contact.last_name = "bb"
+    expect(@contact.valid?).to eq(true)
+  end
+
+  it 'should have email' do 
+    @contact.email = "   "
+    expect(@contact.valid?).to eq(false)
+  end
+
+  it 'should have proper email format' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+
+    valid_addresses.each do |address|
+      @contact.email = address
+      expect(@contact.valid?).to eq(true)
+    end
+  end
+
+  it 'should prevent invalid emails' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                         foo@bar_baz.com foo@bar+baz.com]
+
+    invalid_addresses.each do |address|
+      @contact.email = address
+      expect(@contact.valid?).to eq(false)
+    end
+  end
+
+  it 'should downcase emails before saving' do
+    mixed_email = "AvEnGEr@StarkIndusTRIes.Io"
+
+    @contact.email = mixed_email
+    @contact.save
+
+    @contact.reload
+    expect(@contact.email).to eq(mixed_email.downcase)
+  end
+
+  it 'should have a unique email' do
+    @contact2.email = @contact.email
+    expect(@contact2.valid?).to eq(false)
+  end
+
+  it 'should default partner status to false' do
+    new_contact = Contact.new
+    expect(new_contact.partner).to eq(false)
+  end
+
+  it 'should update partner status' do
+    expect(@contact2.partner).to eq(false)
+
+    @contact2.partner = true
+    expect(@contact2.valid?).to eq(true)
   end
 end
