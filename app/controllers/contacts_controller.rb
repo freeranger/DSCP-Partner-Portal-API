d9d821c4da0bc2cb50922a248832ea0804f8fa88
freeranger <tony.butler@me.com>
2017-03-26 17:21:51 +0100

    Fix Contacts controller issues not using instance variable, return no_content on update, exclude created_at and updated_at from list and get responses

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,39 +4,40 @@ class ContactsController < ApplicationController
 
   def index
     contacts = Contact.all
-    render json: contacts
+    render json: contacts, :except=> [:created_at, :updated_at]
   end
 
   def show
-    render json: contact
+    render json: @contact, :except=> [:created_at, :updated_at]
   end
 
   def create
-    contact = Contact.new(contact_params)
+    @contact = Contact.new(contact_params)
 
-    if contact.save
-      render json: contact, status: :created, location: contact
+    if @contact.save
+      render json: @contact, status: :created, location: @contact
     else
-      render json: contact.errors, status: :unprocessable_entity
+      render json: @contact.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if contact.update(contact_params)
-      render json: contact
+    if @contact.update(contact_params)
+      head :no_content
     else
       render json: contact.errors, status: :unprocessable_entity
     end
+
   end
 
   def destroy
-    contact.destroy
+    @contact.destroy
   end
 
   private
 
     def set_contact
-      contact = Contact.find(params[:id])
+      @contact = Contact.find(params[:id])
     end
 
     def contact_params
