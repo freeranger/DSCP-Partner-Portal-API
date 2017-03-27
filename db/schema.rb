733e3e95e1b8e8284b2725995bbe6dce425613c0
Jake Anderson <andesitedesign@gmail.com>
2017-03-27 13:30:20 -0500

    Prepare app for production

--- db/schema.rb
+++ db/schema.rb
@@ -12,27 +12,30 @@
 
 ActiveRecord::Schema.define(version: 20170315142257) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.integer  "phone",          limit: 8
-    t.integer  "phone_alt",      limit: 8
+    t.bigint   "phone"
+    t.bigint   "phone_alt"
     t.string   "website"
     t.string   "facebook"
     t.string   "instagram"
     t.string   "street_address"
-    t.string   "city",                     default: "St. Charles"
-    t.string   "state",                    default: "IL"
-    t.integer  "zip",            limit: 4
+    t.string   "city",           default: "St. Charles"
+    t.string   "state",          default: "IL"
+    t.integer  "zip"
     t.string   "business_name"
-    t.boolean  "partner",                  default: false
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
-    t.index ["business_name"], name: "index_contacts_on_business_name"
-    t.index ["email"], name: "index_contacts_on_email", unique: true
-    t.index ["first_name"], name: "index_contacts_on_first_name"
-    t.index ["last_name"], name: "index_contacts_on_last_name"
+    t.boolean  "partner",        default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.index ["business_name"], name: "index_contacts_on_business_name", using: :btree
+    t.index ["email"], name: "index_contacts_on_email", unique: true, using: :btree
+    t.index ["first_name"], name: "index_contacts_on_first_name", using: :btree
+    t.index ["last_name"], name: "index_contacts_on_last_name", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
