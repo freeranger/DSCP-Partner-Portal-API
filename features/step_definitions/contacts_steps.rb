a84a08a03e4d61ce6b7e96e911e34b46de226689
freeranger <tony.butler@me.com>
2017-04-13 12:09:12 +0100

    Added tests for location headers to Contacts and Groups

--- features/step_definitions/contacts_steps.rb
+++ features/step_definitions/contacts_steps.rb
@@ -2,6 +2,15 @@ When(/^the system knows about the following contact(?:s)?:$/) do |contact|
   Contact.create!(contact.hashes)
 end
 
+Then(/^the location header points to the created contact/) do
+  contact = last_response.headers['Location']
+  expect(contact).not_to be_nil
+  ids = contact.match(/\/contacts\/(?<id>\d+)/)
+  @contact = Contact.find(ids[:id])
+
+  expect(@contact).not_to be_nil
+end
+
 Then(/^(?:a|the) contact is (?:created|updated) with:$/) do |contacts|
   contacts.hashes.each do |contact|
     expect(Contact.exists?(contact)).to be true
