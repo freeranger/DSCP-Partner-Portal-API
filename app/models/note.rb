ca54f2ffd87875923c8432a8ab30dee61ecd93e5
freeranger <tony.butler@me.com>
2017-04-16 13:44:28 +0100

    Add validates_presence_of to link and note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,7 @@
 class Note < Linkable
   validates :content, presence: true, length: { minimum: 25 }
+  validates_presence_of :user
+  validates_presence_of :group
 
   belongs_to :user
   belongs_to :group
