adf6a3f26e4e28eabd27b6906c2228ca121a1278
Jake Anderson <andesitedesign@gmail.com>
2017-06-15 16:37:10 -0500

    Update db schema via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430091258) do
+ActiveRecord::Schema.define(version: 20170408141330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20170430091258) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["name"], name: "index_groups_on_name", using: :btree
+    t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
   create_table "links", force: :cascade do |t|
