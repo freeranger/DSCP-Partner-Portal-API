c7b91a9ebc18f21a83af2857605018bca19c9985
Jake Anderson <andesitedesign@gmail.com>
2017-07-14 21:42:59 -0500

    Fix origin url for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do  
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000', 'https://dscp-partner-portal.firebaseapp.com'
+    origins 'localhost:3000', '127.0.0.1:3000', 'dscp-partner-portal.firebaseapp.com'
 
     resource '*',
       headers: :any,
